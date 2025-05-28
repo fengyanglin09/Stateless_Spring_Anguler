41d93a6b58a03dfd8b65fc77c54e407ce97ccb75
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-05-28 10:28:25 -0500

    Refactor db fixture to use tmp_path_factory and add tests for tmp_path and tmp_path_factory

--- src/test/fixtures/conftest.py
+++ src/test/fixtures/conftest.py
@@ -6,13 +6,22 @@ import pytest
 import cards
 
 # this will be setup at the beginning of the test session
+# @pytest.fixture(scope="session")
+# def db():
+#     with TemporaryDirectory() as db_dir:
+#         db_path = Path(db_dir) / "cards_db"
+#         db = cards.CardsDB(db_path)
+#         yield db
+#         db.close()
+
 @pytest.fixture(scope="session")
-def db():
-    with TemporaryDirectory() as db_dir:
-        db_path = Path(db_dir) / "cards_db"
-        db = cards.CardsDB(db_path)
-        yield db
-        db.close()
+def db(tmp_path_factory):
+    db_path = tmp_path_factory.mktemp("cards_db")
+    db_ = cards.CardsDB(db_path)
+    yield db_
+    db_.close()
+
+
 
 # this will be setup for each test function to ensure a clean state
 @pytest.fixture(scope="function")
