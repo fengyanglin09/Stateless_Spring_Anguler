81a8805094da1bbfe6187cc973d2b3cfde675776
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-08-21 15:36:18 -0500

    Add new AppButton and AppCard components with SCSS styling and templates

---
+++ frontend-lib/ReadMe/0002ReferenceOtherComponentFromOtherSecondaryEntry.md
@@ -0,0 +1,1 @@
+<name of your lib>/<name of your secondary entry point> is the path to the secondary entry point.
---
+++ frontend-lib/mqml-workspace/projects/shared/ui-sdk/button/public-api.ts
@@ -0,0 +1,3 @@
+export * from './src/app-button.component/app-button.component';
+
+export * from './src/model/app-button.interface';
