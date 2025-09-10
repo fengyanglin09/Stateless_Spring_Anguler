0324607246cf25b02f27cb1b8d5fe514768bc6e3
lin.pengpeng <lin.pengpeng@mayo.edu>
2025-09-10 08:36:53 -0500

    Add Font Awesome integration and enhance app configuration; implement splash screen

--- frontend-lib/mqml-workspace/projects/showcase/src/app/app.config.ts
+++ frontend-lib/mqml-workspace/projects/showcase/src/app/app.config.ts
@@ -1,12 +1,35 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import {MayoTheme} from 'mqml-angular-ui-layout-sdk/layout-template/themes';
+import {MessageService} from 'primeng/api';
+import {TINYMCE_SCRIPT_SRC} from '@tinymce/tinymce-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      }),
+      withEnabledBlockingInitialNavigation()
+    ),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    providePrimeNG({
+      ripple: true,
+      inputStyle: 'filled',
+      theme: MayoTheme
+    }),
+    MessageService,
+    {provide: TINYMCE_SCRIPT_SRC, useValue: "tinymce/tinymce.min.js"},
   ]
 };
+
